1c46f8dffaa21d68f7624f57c1630e567b29dda6
Maxim Kan <mkan@google.com>
2014-06-28 16:10:03 +0400

    exception declaration changed fixed spelling added getElement

--- src/com/google/algebra/matrix/SquareMatrix.java
+++ src/com/google/algebra/matrix/SquareMatrix.java
@@ -16,9 +16,9 @@ public class SquareMatrix {
 	  mElements = new double[dim][dim];
 	}
 	
-	public SquareMatrix(double[][] array) {  	
+	public SquareMatrix(double[][] array) {
 	  if (array[0].length != array.length) {
-		throw new RuntimeException("Input array is not square!");
+		throw new IllegalStateException("Input array is not square!");
 	  }
 	  mElements = new double[array.length][array[0].length];
 	  System.arraycopy(array, 0, mElements, 0, array.length);
@@ -29,6 +29,10 @@ public class SquareMatrix {
 	} 
 	
 	public int getRows() {
-	  return mElements.lenght;
+	  return mElements.length;
+	}
+	
+	public double getElement(int row, int col) {
+	  return mElements[row][col];	
 	}
 }
